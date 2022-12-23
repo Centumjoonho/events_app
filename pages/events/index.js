9e5b81d18f409e0750b12c8633b41e67c542bb2d
Centumjoonho <ghwnsgkgk@kakao.com>
2022-12-23 10:00:36 +0900

    2022-12-23-for-재영소프트웹

---
+++ pages/events/index.js
@@ -0,0 +1,24 @@
+// 색인 페이지 왜 만들지 ? -> http://localhost:3000/events ->  show 페이지
+const Page = () => {
+  return (
+    <div>
+      <h1>Events Page </h1>
+      <div>
+        <a href="/">
+          <img />
+          <h2> 차세대사업</h2>
+        </a>
+        <a href="/">
+          <img />
+          <h2> SI 사업</h2>
+        </a>
+        <a href="/">
+          <img />
+          <h2> 솔루션사업 사업</h2>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
